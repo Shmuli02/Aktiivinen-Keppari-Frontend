facbf7f5b5708ce4e781862076ec2bc9dbc8fe98
Shmuli02 <samuli.kaleva@gmail.com>
2021-06-22 07:56:17 +0000

    instructions added to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,34 @@ import Upload from '../components/Upload'
 
 
 const Home = () => (
-  <div>
-    <h2>Aktiivinen Keppari</h2> 
-    <p>Aktiivinen Keppari kilpailun etusivu</p>
+  <div className="Home">
+    
+    <h1>Aktiivinen keppari kilpailu jatkuu!</h1>
+    <p>Kesän 2021 kilpailuissa keppariharrastajan tehtävänä on suorittaa tehtäväpassin tehtävät kilpailuaikana. Kaikkien osallistujien kesken arvotaan hyviä esinepalkintoja.</p>
+
+    <p><b>Kilpailuaika:</b> 1.7.–31.7.2021</p>
+
+    <p><b>Osallistumisoikeus:</b> Avoin kaikille</p>
+
+    <h4>Palkinnot:</h4>
+    <ol>
+      <li>Kaikkien vähintään yhden tehtävän suorittaneiden kesken arvotaan hyvät esinepalkinnot</li>
+      <li>Bonuspalkinnot arvotaan niiden kesken, jotka ovat suorittaneet kaikki tehtävät kilpailuaikana</li>
+    </ol>
+    <h4>Tehtävät:</h4>
+      <p>Kilpailussa on helppoja ja vaikeita tehtäviä. Osassa tehtäviä osallistujan tulee lähettää kuva tai video, kun suoritus rekisteröidään kilpailusivuille.</p>
+
+    <h4>Säännöt:</h4>
+      <p>Kilpailussa saa tehdä yhteistyötä tehtävien suorittamisessa. Kilpailussa tulee noudattaa tekijänoikeuksia, joten älä kopioi kuvia tai videoita joihin sinulla ei ole oikeuksia.</p>
+
+    <b>Ohjeet osallistumiseen:</b>
+    <ul>
+      <li>Avaa kilpailusivut keppari.com</li>
+      <li>Rekisteröidy sivulle</li>
+      <li>Kun olet tehnyt tehtävän, niin käy rekisteröimässä suorituksesi</li>
+      <li>Kun olet suorittanut kaikki tehtävät, niin paina nappia Tarkista. Saat todistuksen suorituksestasi pari päivän sisällä sähköpostiisi</li>
+      <li>Voit tulostaa itsellesi pdf muodossa tehtäväpassin</li>
+    </ul>
   </div>
 )
 
