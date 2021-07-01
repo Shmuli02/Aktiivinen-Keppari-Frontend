6fdb33e136de2d5c7ec2bfe62d006f1b1ea2b0b7
Shmuli02 <samuli.kaleva@gmail.com>
2021-07-01 09:24:41 +0000

    user name to aws link

--- src/services/upload-files.js
+++ src/services/upload-files.js
@@ -6,6 +6,7 @@ class UploadFilesService {
 
 
     formData.append("file", file);
+    formData.append("user", "user")
     return http.post("/api/upload", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
