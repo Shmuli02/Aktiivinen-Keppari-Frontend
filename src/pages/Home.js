b50603b30cd631fe22add62517c9a907b23d4b41
Shmuli02 <samuli.kaleva@gmail.com>
2021-07-04 08:31:25 +0000

    instructions updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,8 +30,11 @@ const Home = () => (
       <li>Kun olet tehnyt tehtävän, käy rekisteröimässä suoritus <a href='https://aktiivinen-keppari.herokuapp.com/tasks'> Tehtävät sivulle</a></li>
       <li>Kun olet suorittanut kaikki tehtävät, voit ladata todistuksen suorituksestasi pari päivän sisällä </li>
     </ul>
+    <h4>Galleria</h4>
+    <p>Halutessasi voit lisätä kuvan suorituksestasi julkiseen galleriaan</p>
     <h4>Yhteystiedot:</h4>
     <p>Ongalma tilanteissa ota yhteyttä sähköpostitse <a href='mailto:keppari@hewi.fi'>keppari@hewi.fi</a></p>
+    
   </div>
 )
 
